41a178420a5393ce563f6e6d44e6d608e0079251
Lucas Linard <lucas@linard.tech>
2017-12-10 20:58:54 -0200

    Initial testing SETUP.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('<App />', () => {
+    const wrapper = mount(<App />)
+    it('shallow renders without crashing', () => {
+        expect(shallow(<App />))
+    })
+})
